f9d1f3459476ffb13167b68e50d6aa3626cee0ec
naomi <naomi.smulders@student.gildeopleidingen.nl>
2026-06-10 13:37:26 +0200

    code
    hier is de gehele code die is toegevoegd

--- feestdagen.js
+++ feestdagen.js
@@ -1,1 +1,16 @@
+fetch('https://v2.jokeapi.dev/joke/Programming')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
 
+        return response.json();
+    })
+
+    .then(data => {
+        console.log(data);
+    })
+
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
